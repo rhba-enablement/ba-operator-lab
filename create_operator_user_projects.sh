2e6ba551934b87658427a5ef8fcdf5af301c8c40
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-11-12 23:26:45 +0100

    Fixed deployment of business automation operator via CLI.

--- create_operator_user_projects.sh
+++ create_operator_user_projects.sh
@@ -6,7 +6,7 @@
 #declare -a users=("user5" "user6" "user7" "user8" "user9" "user10" "user11" "user12" "user13" "user14" "user15" "user16" "user17" "user18" "user19" "user20" "user21" "user22" "user23" "user24" "user25" "user26" "user27" "user28" "user29" "user30" "user31" "user32" "user33" "user34" "user35" "user36" "user37")
 
 START=1
-END=2
+END=3
 
 echo "Creating projects."
 
@@ -18,8 +18,10 @@ for i in $(seq $START $END); do
    oc policy add-role-to-user admin user$i -n $PROJECT_NAME
 
    # Create Operator subscription.
-   cat subscribe-ba-operator-1.2.0.yaml | sed -e "s/namespace-placeholder/$PROJECT_NAME/g" | oc create -f -
+   cat csc.cr.yaml | sed -e "s/namespace-placeholder/$PROJECT_NAME/g" | oc apply -f -
 
-   # Create the rolebinding to view the app in the Operator Console
-   oc create rolebinding kie-operator-view-kieapps --clusterrole=view-kieapps.app.kiegroup.org-v1 --user=user$i -n $PROJECT_NAME
+   cat ba-operator-sub.yaml | sed -e "s/namespace-placeholder/$PROJECT_NAME/g" | oc apply -f -
+
+   cat operator-group.yaml | sed -e "s/namespace-placeholder/$PROJECT_NAME/g" | oc apply -f -
+
 done
